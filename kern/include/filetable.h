8bbc6879f023c33c883d68063d638f0d843a6385
Josh Wong <joshwong.ca@gmail.com>
2022-10-17 20:36:15 -0700

    Added method to add to open_file_table

--- kern/include/filetable.h
+++ kern/include/filetable.h
@@ -1,15 +1,17 @@
 #include <limits.h>
 #include <types.h>
 #include <vnode.h>
-
+#include <array.h>
 
 struct fd_table {
     struct file_entry* file_entries[OPEN_MAX]; //fd is index
 };
 
 struct open_file_table {
-    struct file_entry* entries[];
+    struct array *entries;
 };
+
+extern struct open_file_table open_file_table;
 
 struct file_entry {
     enum file_status status;
@@ -25,3 +27,4 @@ enum file_status {
 
 struct fd_table *fd_table_create();
 void open_file_table_init(struct open_file_table *ft);
+int open_file_table_add(struct open_file_table *oft, struct file_entry *file_entry);
