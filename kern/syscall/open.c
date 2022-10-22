9b1f0417f56f6acfd52efb291ed208df40910aec
Stanley Zhao <stzhao2@gmail.com>
2022-10-21 17:27:32 -0700

    added copyin to open.c

--- kern/syscall/open.c
+++ kern/syscall/open.c
@@ -4,11 +4,25 @@
 #include <proc.h>
 #include <syscall.h>
 #include <kern/errno.h>
+#include <limits.h>
+#include <copyinout.h>
 
 int
 sys__open(const char *filename, int flags, int *retval) {
+    
+    char *path = kmalloc(PATH_MAX * sizeof(char));
+    size_t *length = kmalloc(sizeof(size_t));
+    int err = copyinstr((const_userptr_t)filename, path, PATH_MAX, length);
+    if (err)
+    {
+        kfree(path);
+        kfree(length);
+        return err;
+    }
     struct vnode *vn = NULL;
-    int err = vfs_open((char *)filename, flags, 0, &vn);
+    err = vfs_open(path, flags, 0, &vn);
+    kfree(path);
+    kfree(length);
     if (err == 0) {
         struct file_entry *file_entry = file_entry_create(flags, 0, vn);
         *retval = fd_table_add(curproc->file_descriptor_table, file_entry);
