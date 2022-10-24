9972b071a0fb2b317430c6e8b47e419a9faab89e
Stanley Zhao <stzhao2@gmail.com>
2022-10-24 01:33:48 -0700

    finished lseek

--- kern/syscall/lseek.c
+++ kern/syscall/lseek.c
@@ -7,6 +7,8 @@
 #include <kern/seek.h>
 #include <limits.h>
 #include <synch.h>
+#include <kern/stat.h>
+#include <vnode.h>
 
 int 
 sys__lseek(int fd, off_t pos, int whence, int *retval)
@@ -48,8 +50,19 @@ sys__lseek(int fd, off_t pos, int whence, int *retval)
         return 0;
     }
     else if (whence == SEEK_END) {
-        // add this
-        return -1;
+        struct stat *stat;
+        int result = VOP_STAT(curproc->file_descriptor_table->file_entries[fd]->file, stat);
+        if (stat->st_size - 1 + pos < 0) {
+            lock_release(curproc->file_descriptor_table->file_entries[fd]->file_entry_lock);
+            kfree(stat);
+            *retval = -1;
+            return EINVAL;
+        }
+        curproc->file_descriptor_table->file_entries[fd]->offset = stat->st_size - 1 + pos;
+        kfree(stat);
+        *retval = curproc->file_descriptor_table->file_entries[fd]->offset;
+        lock_release(curproc->file_descriptor_table->file_entries[fd]->file_entry_lock);
+        return 0;
     }
     else {
         lock_release(curproc->file_descriptor_table->file_entries[fd]->file_entry_lock);
