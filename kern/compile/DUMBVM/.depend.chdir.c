df531efeab6b16b35d9f43b90127d1e558882121
Josh Wong <joshwong.ca@gmail.com>
2022-11-14 21:06:13 -0800

    delete unwanted files

--- kern/compile/DUMBVM/.depend.chdir.c
+++
@@ -1,8 +0,0 @@
-chdir.o: ../../syscall/chdir.c ../../include/filetable.h \
- ../../include/limits.h ../../include/kern/limits.h ../../include/types.h \
- ../../include/kern/types.h includelinks/kern/machine/types.h \
- includelinks/machine/types.h ../../include/vnode.h \
- ../../include/spinlock.h ../../include/cdefs.h \
- includelinks/machine/spinlock.h ../../include/array.h \
- ../../include/lib.h opt-noasserts.h ../../include/vfs.h \
- ../../include/syscall.h ../../include/copyinout.h
